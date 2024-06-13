c7fa678fb644955495cc3eba7af92e4fc74b15c0
Serg1506z <136631034+Serg1506z@users.noreply.github.com>
2024-06-13 17:27:14 +0300

    fixed

--- src/classes/Classes.js
+++ src/classes/Classes.js
@@ -1,47 +1,47 @@
 import Character from "./Character"
 export class Bowerman extends Character {
-  constructor() {
-    super (...arguments)
+  constructor(name, type) {
+    super (name, type)
     this.attack = 25
     this.defence = 25
   }
 }
 
 export class Swordsman extends Character {
-  constructor() {
-    super (...arguments)
+  constructor(name, type) {
+    super (name, type)
     this.attack = 40
     this.defence = 10
   }
 }
 
 export class Magician extends Character {
-  constructor() {
-    super (...arguments)
+  constructor(name, type) {
+    super (name, type)
     this.attack = 10
     this.defence = 40
   }
 }
 
 export class Undead extends Character {
-  constructor() {
-    super (...arguments)
+  constructor(name, type) {
+    super (name, type)
     this.attack = 25
     this.defence = 25
   }
 }
 
 export class Zombie extends Character {
-  constructor() {
-    super (...arguments)
+  constructor(name, type) {
+    super (name, type)
     this.attack = 40
     this.defence = 10
   }
 }
 
 export class Daemon extends Character {
-  constructor() {
-    super (...arguments)
+  constructor(name, type) {
+    super (name, type)
     this.attack = 10
     this.defence = 40
   }
